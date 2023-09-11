51f74b6f3d837fceabdace1847829a242111632b
邝利豪 <10530797+xianianmian@user.noreply.gitee.com>
2023-09-11 21:31:47 +0800

    纯uniapp的

---
+++ myuni3/pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		homefsdfsdf
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
